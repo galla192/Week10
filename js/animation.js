d720f99f2105574ac37c23c23fef727e40a3ca51
galla192 <galla192@msu.edu>
2014-11-11 23:12:54 -0500

    Files for week 10
    files for week 10

---
+++ js/animation.js
@@ -0,0 +1,47 @@
+function hover1(){
+	document.getElementById("pic1").style.width="30%";
+	document.getElementById("pic2").style.width="23%";
+	document.getElementById("pic3").style.width="23%";
+	document.getElementById("pic4").style.width="23%";
+	document.getElementById("heropic").style.visibility="visible";
+	document.getElementById("writeup").style.height="50px";
+	//document.getElementById("heropic").style.height="400px";//
+	document.getElementById("bigpic").src="images/atlas.jpg";
+	document.getElementById("bragging").innerHTML="This is the Atlas robot, I worked on him for a DARPA project (you can hover over to expand)";
+}
+
+function hover2(){
+	document.getElementById("pic1").style.width="23%";
+	document.getElementById("pic2").style.width="30%";
+	document.getElementById("pic3").style.width="23%";
+	document.getElementById("pic4").style.width="23%";
+	document.getElementById("heropic").style.visibility="visible";
+	document.getElementById("writeup").style.height="50px";
+	//document.getElementById("heropic").style.height="400px";
+	document.getElementById("bigpic").src="images/game.png";
+	document.getElementById("bragging").innerHTML="This is a screenshot of the start screen for a game I've been working on in another class";
+}
+
+function hover3(){
+	document.getElementById("pic1").style.width="23%";
+	document.getElementById("pic2").style.width="23%";
+	document.getElementById("pic3").style.width="30%";
+	document.getElementById("pic4").style.width="23%";
+	document.getElementById("heropic").style.visibility="visible";
+	document.getElementById("writeup").style.height="50px";
+	//document.getElementById("heropic").style.height="400px";
+	document.getElementById("bigpic").src="images/msu.png";
+	document.getElementById("bragging").innerHTML="I spent a year and a half away from home but I came to my senses and came back to MSU";
+}
+
+function hover4(){
+	document.getElementById("pic1").style.width="23%";
+	document.getElementById("pic2").style.width="23%";
+	document.getElementById("pic3").style.width="23%";
+	document.getElementById("pic4").style.width="30%";
+	document.getElementById("heropic").style.visibility="visible";
+	document.getElementById("writeup").style.height="50px";
+	//document.getElementById("heropic").style.height="400px";
+	document.getElementById("bigpic").src="images/ball.png";
+	document.getElementById("bragging").innerHTML="I love to play soccer as often as possible, too much sometimes";
+}
